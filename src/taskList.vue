e872647a84ce2df5a30ee6541553da1033de6995
ishsgit <aishtahad@yahoo.com>
2024-10-19 19:47:56 -0400

    task form

---
+++ src/taskList.vue
@@ -0,0 +1,30 @@
+// TaskList.vue
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TaskList',
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+    },
+    toggleTaskCompletion: {
+      type: Function,
+      required: true,
+    },
+    deleteTask: {
+      type: Function,
+      required: true,
+    },
+  },
+  setup(props) {
+    return () => (
+      <ul class="task-list">
+        {props.tasks.map((task, index) => (
+     
+          </li>
+        ))}
+      </ul>
+    );
+  },
+});

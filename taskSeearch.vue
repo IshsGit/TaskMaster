ad5145e74532373cc4407dfd9c8bb062569fac36
ishsgit <aishtahad@yahoo.com>
2024-10-21 12:00:00 -0400

    onSearch was changed to emitSearch

--- taskSeearch.vue
+++ taskSeearch.vue
@@ -1,32 +1,31 @@
 <template>
-    <div class="task-search">
-      <input v-model="searchQuery" @input="emitSearch" placeholder="Search tasks..." />
-    </div>
-  </template>
-  
-  <script>
-  import { defineComponent, ref } from 'vue';
-  
-  export default defineComponent({
-    name: 'TaskSearch',
-    emits: ['search'],
-    setup(_, { emit }) {
-      const searchQuery = ref('');
-  
-      const emitSearch = () => emit('search', searchQuery.value);
-  
-      return {
-        searchQuery,
-        emitSearch,
-      };
-    },
-  });
-  </script>
-  
-  <style scoped>
-  .task-search input {
-    width: 100%;
-    padding: 8px;
-  }
-  </style>
-  
+  <div class="task-search">
+    <input v-model="searchQuery" @input="emitSearch" placeholder="Search tasks..." />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'TaskSearch',
+  emits: ['search'],
+  setup(_, { emit }) {
+    const searchQuery = ref('');
+
+    const emitSearch = () => emit('search', searchQuery.value);
+
+    return {
+      searchQuery,
+      emitSearch,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.task-search input {
+  width: 100%;
+  padding: 8px;
+}
+</style>
